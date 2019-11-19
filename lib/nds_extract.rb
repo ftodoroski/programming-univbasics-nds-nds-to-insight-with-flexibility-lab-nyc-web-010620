db49f2d7b333932e6bf7137c91c90d733260f505
Filip Todoroski <ftodoroski@ymail.com>
2019-11-19 22:54:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -72,7 +72,6 @@ def movies_with_directors_set(source)
     while j < director_movies.length
       movie = director_movies[j]
       movie[:director_name] = director_name
-      pp movie
       directors << movie
 
 
