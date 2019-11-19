65e8cd363d74f1f65b6ce9049950cdd79a4f933d
Filip Todoroski <ftodoroski@ymail.com>
2019-11-19 22:23:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -72,7 +72,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  pp source
+  # pp source
 end
 
 # ----------------    End of Your Code Region --------------------
