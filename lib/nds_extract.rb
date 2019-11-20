34e091583f55968f3d3368f300c11c02a7a66308
Filip Todoroski <ftodoroski@ymail.com>
2019-11-20 01:12:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -62,18 +62,6 @@ def gross_per_studio(collection)
   gross_studio
 end
 
-# GOAL: Given an Array of Hashes where each Hash represents a movie,
-# return a Hash that includes the total worldwide_gross of all the movies from
-# each studio.
-#
-# INPUT:
-# * collection: Array of Hashes where each Hash where each Hash represents a movie
-#
-# RETURN:
-#
-# Hash whose keys are the studio names and whose values are the sum
-# total of all the worldwide_gross numbers for every movie in the input Hash
-
 def movies_with_directors_set(source)
   movies = []
 
